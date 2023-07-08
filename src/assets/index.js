3308c86fb8d2e5e67769b5fada3118f9a1e92776
SHAURYA AWASTHI <89693063+Shauryaa007@users.noreply.github.com>
2023-07-08 12:23:41 +0530

    task one done

---
+++ src/assets/index.js
@@ -0,0 +1,22 @@
+import logo from "./images/logo.png";
+import bannerImg from "./images/bannerImg.png";
+import blood from "./images/projects/blood.png";
+import eportfolio from "./images/projects/eportfolio.png";
+import text from "./images/projects/text.png";
+import pixel from "./images/projects/pixel.png";
+import major from "./images/projects/major.png";
+import Sharp from "./images/projects/Sharp.png";
+import testimonialOne from "./images/testmonial/testimonialOne.png";
+import testimonialTwo from "./images/testmonial/testimonialTwo.png";
+import quote from "./images/testmonial/quote.png";
+import contactImg from "./images/contact/contactImg.png";
+
+export {
+  logo,
+  bannerImg,
+ text,pixel,blood,major,Sharp,eportfolio,
+  testimonialOne,
+  testimonialTwo,
+  quote,
+  contactImg,
+};
